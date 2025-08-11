d33bd5e64921a601251bf9fc7590bb0ac826db7b
Juan Felipe Rada <radapls8@gmail.com>
2025-08-11 11:39:39 -0500

    feat: Adjust pipeline.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -5,7 +5,6 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'production',
